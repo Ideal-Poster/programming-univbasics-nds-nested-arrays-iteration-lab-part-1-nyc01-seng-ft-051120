abbfd60bd02c94673b5841ba3812a1e8d0425d64
Malcolm Gourdine <malcolmgourdine@protonmail.com>
2020-04-23 21:14:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,5 +2,20 @@ def find_even_values(src)
   # Given an Array of Arrays of Integers ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ):
   #
   # Output all even values in each nested array
+  return_array = []
+  outerCount = 0
+  innerCount = 0
 
+  binding.irb
+  
+  while src[outerCount]
+    while src[outerCount][innerCount]
+      currentNumber = src[outerCount][innerCount]
+      return_array << currentNumber if currentNumber % 2 == 0
+      innerCount += 1
+    end
+    innerCount = 0
+    outerCount += 1
+  end
+  return_array
 end
